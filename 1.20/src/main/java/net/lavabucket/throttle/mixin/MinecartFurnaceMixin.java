b0254af9fafe4142ea761cf98c6a4e23151cfc66
ThePegNok <thepegnok@gmail.com>
2026-03-09 21:28:06 -0300

    Added 1.20 compatibility files (to the best of my ability)

---
+++ 1.20/src/main/java/net/lavabucket/throttle/mixin/MinecartFurnaceMixin.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021 Nick Iacullo
+ *
+ * This file is part of Throttle.
+ *
+ * Throttle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Throttle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Throttle.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.lavabucket.throttle.mixin;
+
+import net.minecraft.world.entity.vehicle.MinecartFurnace;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * The mixin for modifying furnace minecart behavior.
+ */
+@Mixin(MinecartFurnace.class)
+public class MinecartFurnaceMixin {
+
+    /**
+     * Increases default minecart speed so it matches The one from throttle when configured
+     */
+    @Inject(method = "getMaxFurnaceSpeedWithRail", at = @At("HEAD"), cancellable=true, remap=false)
+    private void getMaxFurnaceSpeedWithRail(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(0.8f)
+    }
+}
